a4390e59ff54c263fd0a5a54557f63f162a7acac
Andres Lerner <38118136+AndresL99@users.noreply.github.com>
2021-09-01 09:37:37 -0300

    Added PetTypeService. Closes #29.

---
+++ pet-clinic-data/src/main/java/guro/springframework/sfgpetclinic/service/PetTypeService.java
@@ -0,0 +1,8 @@
+package guro.springframework.sfgpetclinic.service;
+
+import guro.springframework.sfgpetclinic.domain.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long>
+{
+
+}
