2ac62d969344b0f1eb9f45328906eafb3a14fb4e
Andres Lerner <38118136+AndresL99@users.noreply.github.com>
2021-12-11 09:08:32 -0300

    Updated Vet with JPA entity. Closes #37.

--- pet-clinic-data/src/main/java/guro/springframework/sfgpetclinic/domain/Speciality.java
+++ pet-clinic-data/src/main/java/guro/springframework/sfgpetclinic/domain/Speciality.java
@@ -1,8 +1,18 @@
 package guro.springframework.sfgpetclinic.domain;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity
 {
+
+    @Column(name = "description")
     private String description;
+
 
     public String getDescription() {
         return description;
